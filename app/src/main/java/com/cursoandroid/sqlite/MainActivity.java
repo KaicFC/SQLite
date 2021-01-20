f6974e0c454d952ecae3a9ce75d4c22edf0bafbb
Kaic Carvalho <kaic15_fernandes@hotmail.com>
2021-01-20 20:45:10 -0300

    Inplementado o ID com a autoincremento.

--- app/src/main/java/com/cursoandroid/sqlite/MainActivity.java
+++ app/src/main/java/com/cursoandroid/sqlite/MainActivity.java
@@ -19,21 +19,28 @@ public class MainActivity extends AppCompatActivity {
             //Criar ou Abrir o Banco de Dados
             SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
+            //Excluir a Tabela
+            //bancoDados.execSQL("DROP TABLE pessoas");
+
             //Criar Tabela
-            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (nome VARCHAR, idade INT(3))");
+            bancoDados.execSQL("CREATE TABLE IF NOT EXISTS pessoas (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3))");
 
             //Inserir dados
             bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Kaic', 23)");
             bancoDados.execSQL("INSERT INTO pessoas(nome, idade) VALUES ('Morgana', 25)");
 
+            //Atualizando dados
+            bancoDados.execSQL("UPDATE pessoas SET idade = 28 WHERE nome = 'Morgana'");
+
             //Recuperar dados
-            String consulta = "SELECT nome, idade FROM pessoas " +
+            String consulta = "SELECT id, nome, idade FROM pessoas " +
                                 "WHERE idade BETWEEN  20 AND 30 ORDER BY nome ASC LIMIT 2";
 
             //Cursor percorre os dados
             Cursor cursor = bancoDados.rawQuery(consulta, null);
 
             //Indices da tabela
+            int indiceId = cursor.getColumnIndex("id");
             int indiceNome = cursor.getColumnIndex("nome");
             int indiceIdade = cursor.getColumnIndex("idade");
 
@@ -45,8 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
                 String nome = cursor.getString(indiceNome);
                 String idade = cursor.getString(indiceIdade);
+                String id = cursor.getString(indiceId);
 
-                 Log.i("RESULTADO - nome ",  nome + "idade: " + idade);
+                 Log.i("RESULTADO - nome ",  nome + ", idade: " + idade + ", id" + id);
                 cursor.moveToNext();
              }
 
